77f79e81adb3d41cbbd85383968fe6e4216edfe2
RadosRosic <rados.rosic@gmail.com>
2023-02-07 21:01:25 +0100

    remove unused useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import CombinationContext from "./context/combination-context";
 import EndGamePage from "./pages/EndGamePage";
 
